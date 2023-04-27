bd8d4756bb1077c7e412980880bb785ee8b20fa8
eunjoo0311 <yyy7423@naver.com>
2023-04-27 16:00:39 +0900

    0427

---
+++ js/index.js
@@ -0,0 +1,8 @@
+import ColaGenerator from "./classes/colaGenerator.js";
+import VendingMachineEvents from "./classes/vendingMachineEvents.js";
+
+const colaGenerator = new ColaGenerator();
+const vendingMachineEvents = new VendingMachineEvents();
+
+colaGenerator.setup();
+vendingMachineEvents.bindEvent();
